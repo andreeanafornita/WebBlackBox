d940cf145435e39f0e5eb6c2e03bf1fc0278b313
andreeanafornita <nafornita.andrea42@gmail.com>
2024-09-19 22:33:16 +0300

    am reparat modalul pe webdesign sa vad daca merge asa

--- src/pages/WebDesign.js
+++ src/pages/WebDesign.js
@@ -1,14 +1,47 @@
+// WebDesign.js
+
 import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPalette, faPencilRuler, faCode, faPaintBrush, faCheck, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import {
+  faPalette,
+  faPencilRuler,
+  faCode,
+  faPaintBrush,
+  faCheck,
+  faArrowRight,
+  faAngleLeft,
+  faAngleRight,
+} from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
 import { useInView } from 'react-intersection-observer';
 import { Fade, Slide } from 'react-awesome-reveal';
-import { useTranslation } from 'react-i18next'; // Import useTranslation hook
+import { useTranslation } from 'react-i18next';
 
+// Import all images for each category
 import clinicaeng1 from '../assets/clinica/clinicaeng1.png';
+import clinicaeng2 from '../assets/clinica/clinicaeng2.png';
+import clinicaeng3 from '../assets/clinica/clinicaeng3.png';
+import clinicaeng4 from '../assets/clinica/clinicaeng4.png';
+import clinicaeng5 from '../assets/clinica/clinicaeng5.png';
+
 import coaforeng1 from '../assets/salon/coaforeng1.png';
+import coaforeng2 from '../assets/salon/coaforeng2.png';
+import coaforeng3 from '../assets/salon/coaforeng3.png';
+import coaforeng4 from '../assets/salon/coaforeng4.png';
+import coaforeng5 from '../assets/salon/coaforeng5.png';
+import coaforeng6 from '../assets/salon/coaforeng6.png';
+import coaforeng7 from '../assets/salon/coaforeng7.png';
+import coaforeng8 from '../assets/salon/coaforeng8.png';
+
 import hoteleng1 from '../assets/hotel/hoteleng1.png';
+import hoteleng2 from '../assets/hotel/hoteleng2.png';
+import hoteleng3 from '../assets/hotel/hoteleng3.png';
+import hoteleng4 from '../assets/hotel/hoteleng4.png';
+import hoteleng5 from '../assets/hotel/hoteleng5.png';
+import hoteleng6 from '../assets/hotel/hoteleng6.png';
+import hoteleng7 from '../assets/hotel/hoteleng7.png';
+import hoteleng8 from '../assets/hotel/hoteleng8.png';
+import { color } from 'framer-motion';
 
 export default function WebDesign() {
   const navigate = useNavigate();
@@ -16,7 +49,34 @@ export default function WebDesign() {
 
   const [isSmallScreen, setIsSmallScreen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [modalImages, setModalImages] = useState([]);
+  const [modalImage, setModalImage] = useState(null);
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+
+  // Create a categories object
+  const categories = {
+    clinica: [clinicaeng1, clinicaeng2, clinicaeng3, clinicaeng4, clinicaeng5],
+    coafor: [
+      coaforeng1,
+      coaforeng2,
+      coaforeng3,
+      coaforeng4,
+      coaforeng5,
+      coaforeng6,
+      coaforeng7,
+      coaforeng8,
+    ],
+    hotel: [
+      hoteleng1,
+      hoteleng2,
+      hoteleng3,
+      hoteleng4,
+      hoteleng5,
+      hoteleng6,
+      hoteleng7,
+      hoteleng8,
+    ],
+  };
 
   useEffect(() => {
     const handleResize = () => {
@@ -28,6 +88,7 @@ export default function WebDesign() {
 
     return () => window.removeEventListener('resize', handleResize);
   }, []);
+
   const styles = {
     container: {
       color: 'white',
@@ -40,7 +101,7 @@ export default function WebDesign() {
       color: '#ae8507',
       textAlign: 'center',
       marginBottom: '2rem',
-      fontSize: isSmallScreen?'3.5rem':'5.5rem',
+      fontSize: isSmallScreen ? '3.5rem' : '5.5rem',
     },
     headerSection: {
       textAlign: 'center',
@@ -48,11 +109,11 @@ export default function WebDesign() {
       borderBottom: '1px solid #333',
     },
     headerTitle: {
-      fontSize: isSmallScreen?'2rem':'4rem',
+      fontSize: isSmallScreen ? '2rem' : '4rem',
       marginBottom: '1rem',
     },
     headerSubtitle: {
-      fontSize: isSmallScreen?'1.5rem':'2rem',
+      fontSize: isSmallScreen ? '1.5rem' : '2rem',
       marginBottom: '5rem',
     },
     ctaButton: {
@@ -61,7 +122,7 @@ export default function WebDesign() {
       padding: '1rem 2rem',
       border: 'none',
       borderRadius: '5px',
-      fontSize: isSmallScreen?'1.5rem':'2rem',
+      fontSize: isSmallScreen ? '1.5rem' : '2rem',
       cursor: 'pointer',
       marginBottom: '5rem',
     },
@@ -74,17 +135,18 @@ export default function WebDesign() {
       overflowX: 'hidden',
     },
     sectionTitle: {
-      fontSize: isSmallScreen?'2rem':'2.5rem',
+      fontSize: isSmallScreen ? '2rem' : '2.5rem',
       marginBottom: '5rem',
       marginTop: '5rem',
       textAlign: 'center',
     },
     portfolioGrid: {
       display: 'flex',
-      justifyContent: 'center',
+      justifyContent: isSmallScreen?'flex-start':'center',
       gap: '1rem',
       marginBottom: '1rem',
       width: '100%',
+      flexWrap: isSmallScreen ? 'nowrap' : 'nowrap',
     },
     portfolioImage: {
       width: '250px',
@@ -102,33 +164,63 @@ export default function WebDesign() {
       left: 0,
       width: '100%',
       height: '100%',
-      backgroundColor: 'rgba(0, 0, 0, 0.8)',
+      backgroundColor: 'rgba(0, 0, 0, 0.95)',
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
       zIndex: 1000,
+      padding: '1rem',
     },
     modalContent: {
       position: 'relative',
-      width: '80%',
+      width: '100%',
       maxWidth: '800px',
       maxHeight: '90%',
       display: 'flex',
-      justifyContent: 'center',
+      flexDirection: 'column',
       alignItems: 'center',
     },
     modalImage: {
       width: '100%',
       height: 'auto',
+      maxHeight: '70vh',
+      objectFit: 'contain',
     },
     closeButton: {
       position: 'absolute',
       top: '10px',
-      right: '10px',
+      right: '-30px',
       background: 'transparent',
       border: 'none',
       color: '#fff',
       fontSize: '3rem',
+      cursor: 'pointer',
+      color:'#ae8507',
+      
+    },
+    arrow: {
+      position: 'absolute',
+      top: '50%',
+      transform: 'translateY(-50%)',
+      fontSize: '5rem',
+      color: '#fff',
+      cursor: 'pointer',
+      zIndex: 1001,
+      color:'#ae8507',
+    },
+    gallery: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: '1rem',
+      gap: '0.5rem',
+      overflowY: 'auto',
+      maxHeight: '100px',
+    },
+    galleryImage: {
+      width: '80px',
+      height: '60px',
+      objectFit: 'cover',
       cursor: 'pointer',
     },
     processSection: {
@@ -145,17 +237,17 @@ export default function WebDesign() {
       justifyContent: isSmallScreen ? 'center' : 'space-between',
       gap: '2rem',
       marginTop: '2rem',
-      alignItems: 'flex-start', // Aligns all items to the top
+      alignItems: 'flex-start',
       flexDirection: isSmallScreen ? 'column' : 'row',
     },
     step: {
-      width: isSmallScreen ? '100%' : '22%', // Full width on small screens
+      width: isSmallScreen ? '100%' : '22%',
       textAlign: 'center',
-      marginLeft:'2rem',
+      marginLeft: '2rem',
     },
     stepTitle: {
       marginBottom: '1rem',
-      minHeight: '5.5rem', // Ensures all titles have the same height
+      minHeight: '5.5rem',
     },
     stepText: {
       textAlign: 'left',
@@ -170,7 +262,7 @@ export default function WebDesign() {
       padding: 0,
       marginTop: '1rem',
       lineHeight: '2.5',
-      fontSize: isSmallScreen?'1.5rem':'2rem',
+      fontSize: isSmallScreen ? '1.5rem' : '2rem',
       marginBottom: '5rem',
       marginTop: '5rem',
     },
@@ -184,6 +276,7 @@ export default function WebDesign() {
       justifyContent: 'space-around',
       alignItems: 'center',
       marginTop: '3rem',
+      flexDirection: isSmallScreen ? 'column' : 'row',
     },
     testimonial: {
       textAlign: 'center',
@@ -194,22 +287,15 @@ export default function WebDesign() {
       color: '#fff',
       maxWidth: '300px',
       fontSize: isSmallScreen ? '1.5rem' : '2rem',
-
-    },
-    clientImage: {
-      borderRadius: '50%',
-      width: '80px',
-      height: '80px',
-      marginBottom: '1rem',
-    },
-    testimonialText: {
-      fontStyle: 'italic',
-      marginBottom: '1rem',
-      fontSize: '1.5rem',
     },
     clientName: {
       fontWeight: 'bold',
       marginTop: '0.5rem',
+      fontSize: '1.5rem',
+    },
+    testimonialText: {
+      fontStyle: 'italic',
+      marginBottom: '1rem',
       fontSize: '1.5rem',
     },
     faqSection: {
@@ -219,41 +305,57 @@ export default function WebDesign() {
     },
     faqItem: {
       marginBottom: '1.5rem',
-      fontSize: isSmallScreen?'1.5rem':'2rem',
-
+      fontSize: isSmallScreen ? '1.5rem' : '2rem',
     },
     footer: {
       textAlign: 'center',
       padding: '2rem 0',
       borderTop: '1px solid #333',
-      fontSize: isSmallScreen?'1.5rem':'2.5rem',
+      fontSize: isSmallScreen ? '1.5rem' : '2.5rem',
       marginBottom: '10rem',
       marginTop: '10rem',
     },
   };
-  
+
   const handleNavigateToPortfolio = () => {
     navigate('/portfolio');
-    
   };
 
   const handleNavigateToContact = () => {
     navigate('/contact');
-    
   };
 
-  const openImageModal = (image) => {
-    setSelectedImage(image);
+  // Modify the openImageModal function
+  const openImageModal = (category, index) => {
+    setModalImages(categories[category]);
+    setModalImage(categories[category][index]);
+    setSelectedImageIndex(index);
     setIsModalOpen(true);
     document.body.style.overflow = 'hidden';
     document.body.style.overflowX = 'hidden';
   };
 
+  // Update the closeModal function
   const closeModal = () => {
     setIsModalOpen(false);
-    setSelectedImage(null);
+    setModalImage(null);
+    setSelectedImageIndex(0);
     document.body.style.overflow = 'auto';
     document.body.style.overflowX = 'hidden';
+  };
+
+  // Navigation functions
+  const handleNext = () => {
+    const nextIndex = (selectedImageIndex + 1) % modalImages.length;
+    setSelectedImageIndex(nextIndex);
+    setModalImage(modalImages[nextIndex]);
+  };
+
+  const handlePrev = () => {
+    const prevIndex =
+      (selectedImageIndex - 1 + modalImages.length) % modalImages.length;
+    setSelectedImageIndex(prevIndex);
+    setModalImage(modalImages[prevIndex]);
   };
 
   const { ref: headerRef, inView: headerInView } = useInView({ triggerOnce: true });
@@ -266,7 +368,7 @@ export default function WebDesign() {
 
   return (
     <div style={styles.container}>
-      <h1 style={styles.mainTitle}>{t("webDesignPage.title")}</h1>
+      <h1 style={styles.mainTitle}>{t('webDesignPage.title')}</h1>
 
       <section
         ref={headerRef}
@@ -277,13 +379,11 @@ export default function WebDesign() {
         }}
       >
         <h1 style={styles.headerTitle}>
-          <span>{t("webDesignPage.subTitle")}</span>
+          <span>{t('webDesignPage.subTitle')}</span>
         </h1>
-        <p style={styles.headerSubtitle}>
-          {t("webDesignPage.description")}
-        </p>
+        <p style={styles.headerSubtitle}>{t('webDesignPage.description')}</p>
         <button style={styles.ctaButton} onClick={handleNavigateToContact}>
-          {t("webDesignPage.ctaButton")}
+          {t('webDesignPage.ctaButton')}
         </button>
       </section>
 
@@ -296,29 +396,75 @@ export default function WebDesign() {
           overflowX: isSmallScreen ? 'auto' : 'visible',
         }}
       >
-        <h2 style={styles.sectionTitle}>{t("webDesignPage.portfolioTitle")}</h2>
+        <h2 style={styles.sectionTitle}>{t('webDesignPage.portfolioTitle')}</h2>
         <div
           style={{
             ...styles.portfolioGrid,
-            display: isSmallScreen ? 'flex' : 'flex',
             overflowX: isSmallScreen ? 'scroll' : 'hidden',
             whiteSpace: isSmallScreen ? 'nowrap' : 'normal',
           }}
         >
-          <img src={clinicaeng1} alt="Web Design Project 1" style={styles.portfolioImage} onClick={() => openImageModal(clinicaeng1)} />
-          <img src={coaforeng1} alt="Web Design Project 2" style={styles.portfolioImage} onClick={() => openImageModal(coaforeng1)} />
-          <img src={hoteleng1} alt="Web Design Project 3" style={styles.portfolioImage} onClick={() => openImageModal(hoteleng1)} />
+          {/* Adjust the onClick handlers */}
+          <img
+            src={clinicaeng1}
+            alt="Web Design Project 1"
+            style={styles.portfolioImage}
+            onClick={() => openImageModal('clinica', 0)}
+          />
+          <img
+            src={coaforeng1}
+            alt="Web Design Project 2"
+            style={styles.portfolioImage}
+            onClick={() => openImageModal('coafor', 0)}
+          />
+          <img
+            src={hoteleng1}
+            alt="Web Design Project 3"
+            style={styles.portfolioImage}
+            onClick={() => openImageModal('hotel', 0)}
+          />
         </div>
-        <p style={styles.portfolioLink} onClick={handleNavigateToPortfolio}>{t("webDesignPage.viewAllDesigns")}</p>
+        <p style={styles.portfolioLink} onClick={handleNavigateToPortfolio}>
+          {t('webDesignPage.viewAllDesigns')}
+        </p>
       </section>
 
+      {/* Update the modal rendering */}
       {isModalOpen && (
         <div style={styles.modalOverlay} onClick={closeModal}>
           <div style={styles.modalContent} onClick={(e) => e.stopPropagation()}>
-            <img src={selectedImage} alt="Enlarged View" style={styles.modalImage} />
             <button style={styles.closeButton} onClick={closeModal}>
               &times;
             </button>
+            <FontAwesomeIcon
+              icon={faAngleLeft}
+              onClick={handlePrev}
+              style={{ ...styles.arrow, left: '10px' }}
+            />
+            <img src={modalImage} alt="Enlarged View" style={styles.modalImage} />
+            <FontAwesomeIcon
+              icon={faAngleRight}
+              onClick={handleNext}
+              style={{ ...styles.arrow, right: '10px' }}
+            />
+            <div style={styles.gallery}>
+              {modalImages.map((image, index) => (
+                <img
+                  key={index}
+                  src={image}
+                  alt={`Image ${index + 1}`}
+                  style={{
+                    ...styles.galleryImage,
+                    border:
+                      selectedImageIndex === index ? '2px solid #ae8507' : 'none',
+                  }}
+                  onClick={() => {
+                    setSelectedImageIndex(index);
+                    setModalImage(image);
+                  }}
+                />
+              ))}
+            </div>
           </div>
         </div>
       )}
